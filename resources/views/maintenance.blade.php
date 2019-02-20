582ccfe2f8cc8ddf2ea3032611848612621aae8b
Sai Allu <allu@usc.edu>
2019-02-19 22:45:31 -0800

    Done with Assignment 4

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <title>Week 3</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="jquery_script.js"></script>
-
-</head>
-<body>
 
    @extends('layout')
 
@@ -16,8 +5,5 @@
     @section('main')
 
 	<p> Site is in maintenance mode, thank you! </p>
-
-
-</body>
-</html>
+    @endsection
 
